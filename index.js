86b3a55c8d99a429d8a5234a4bf4943a981ac89a
Dimitri Harding <vaughnharding@hotmail.com>
2018-06-09 15:42:42 -0700

    refactor(mrt): allow tests with two urls to create expected and actual files

--- index.js
+++ index.js
@@ -12,111 +12,120 @@ const METADATA_PATH = 'tests/metadata-regression';
 let options = {};
 
 const youHaveBeenBad = (msg, err) => {
-		throw new Error(msg, err);
+	throw new Error(msg, err);
 };
 
 const createFilenameFromUrl = url => {
-		let filename = url
-				.replace(/^.*\/\/[^/]+/, '')
-				.replace(/[^\w\s]/gi, '_');
-		const homepage = '_homepage';
-		if (filename === '') {
-				filename = homepage;
-		}
+	let filename = url
+		.replace(/^.*\/\/[^/]+/, '')
+		.replace(/[^\w\s]/gi, '_');
+	const homepage = '_homepage';
+	if (filename === '') {
+		filename = homepage;
+	}
 
-		if (filename === '_') {
-				filename = homepage;
-		}
+	if (filename === '_') {
+		filename = homepage;
+	}
 
-		return filename;
+	return filename;
 };
 
 const compareMetaData = (testMetadata, referenceMetadata, diffPath) => {
-		const diffResults = jsonDiffString(testMetadata, referenceMetadata, {}, options);
+	const diffResults = jsonDiffString(testMetadata, referenceMetadata, {}, options);
 
-		if (diffResults === '') {
-				log.info('SUCCESS', `Regression Testing was successful for: ${log.link(diffPath.replace('/diffs/', '/actual/'))}`);
-				return true;
-		}
+	if (diffResults === '') {
+		log.info('SUCCESS', `Regression Testing was successful for: ${log.link(diffPath.replace('/diffs/', '/actual/'))}`);
+		return true;
+	}
 
-		// Log difference
-		log.warn('DIFFERENCE', `${chalk.green('Green = Expected')} | ${chalk.red('Red = Current')}`);
-		console.info(diffResults);
+	// Log difference
+	log.warn('DIFFERENCE', `${chalk.green('Green = Expected')} | ${chalk.red('Red = Current')}`);
+	console.info(diffResults);
 
-		// Remove ascii codes
-		fs.outputFileSync(`${diffPath}.txt`, diffResults.replace(/\[31m|\[39m|\[32m|/g, ''));
+	// Remove ascii codes
+	fs.outputFileSync(`${diffPath}.txt`, diffResults.replace(/\[31m|\[39m|\[32m|/g, ''));
 
-		// Log path for results
-		log.off('DIFF PATH', `${log.link(diffPath)}`);
+	// Log path for results
+	log.off('DIFF PATH', `${log.link(diffPath)}`);
 
-		return false;
+	return false;
 };
 
 const getMetadatDiff = (expected, actual) => {
-		const expectedJSON = fs.readJsonSync(expected);
-		const actualJSON = fs.readJsonSync(actual);
-		const diffPath = expected.replace('/expected/', '/diffs/');
-		return compareMetaData(actualJSON, expectedJSON, diffPath);
+	const expectedJSON = fs.readJsonSync(expected);
+	const actualJSON = fs.readJsonSync(actual);
+	const diffPath = expected.replace('/expected/', '/diffs/');
+	return compareMetaData(actualJSON, expectedJSON, diffPath);
 };
 
 const generateMetaFiles = fetchedMetadata => {
-		const [TEST_URL,
-				REF_URL] = Object.keys(fetchedMetadata);
-		const filename = createFilenameFromUrl(TEST_URL);
-		// Check for ref
-		if (REF_URL !== undefined) {
-				const [,
-						testDomain] = TEST_URL.match(DOMAIN_REG);
-				const [,
-						refDomain] = REF_URL.match(DOMAIN_REG);
-				const diffPath = `${options.path}/diffs/${testDomain}_VS_${refDomain}${filename}.json`;
+	let expectedMetaPath = '';
+	let actualMetaPath = '';
+	const [TEST_URL,
+		REF_URL] = Object.keys(fetchedMetadata);
+	let filename = createFilenameFromUrl(TEST_URL);
+	// Check for ref
+	if (REF_URL !== undefined) {
+		const [,
+			testDomain] = TEST_URL.match(DOMAIN_REG);
+		const [,
+			refDomain] = REF_URL.match(DOMAIN_REG);
 
-				return compareMetaData(fetchedMetadata[TEST_URL], fetchedMetadata[REF_URL], diffPath);
-		}
-		const expectedMetaPath = `${options.path}/expected/${filename}.json`;
-		const actualMetaPath = `${options.path}/actual/${filename}.json`;
+		filename = `${testDomain}_VS_${refDomain}${filename}`;
+		expectedMetaPath = `${options.path}/expected/${filename}.json`;
+		actualMetaPath = `${options.path}/actual/${filename}.json`;
 
-		// Create actual metadata file with data
+		// Create expected and actual metadata file with data
+		fs.outputFileSync(expectedMetaPath, JSON.stringify(fetchedMetadata[REF_URL], null, '\t'));
 		fs.outputFileSync(actualMetaPath, JSON.stringify(fetchedMetadata[TEST_URL], null, '\t'));
-
-		// Create new expected metadata json file if none exists
-		if (!fs.existsSync(expectedMetaPath)) {
-				log.off('MRT', 'Expected metadata JSON does NOT exist.');
-				log.off('MRT', 'Creating Expected metadata JSON from Result: ' + log.link(expectedMetaPath));
-
-				try {
-						fs.copySync(actualMetaPath, expectedMetaPath);
-						log.off('MRT', 'Expected Metadata file created!');
-				} catch (err) {
-						/* istanbul ignore next */
-						youHaveBeenBad('🤔 Something went wrong while copying metadata - Check actual folder', err);
-				}
-		}
-
 		return getMetadatDiff(expectedMetaPath, actualMetaPath);
+	}
+
+	expectedMetaPath = `${options.path}/expected/${filename}.json`;
+	actualMetaPath = `${options.path}/actual/${filename}.json`;
+
+	// Create actual metadata file with data
+	fs.outputFileSync(actualMetaPath, JSON.stringify(fetchedMetadata[TEST_URL], null, '\t'));
+
+	// Create new expected metadata json file if none exists
+	if (!fs.existsSync(expectedMetaPath)) {
+		log.off('MRT', 'Expected metadata JSON does NOT exist.');
+		log.off('MRT', 'Creating Expected metadata JSON from Result: ' + log.link(expectedMetaPath));
+
+		try {
+			fs.copySync(actualMetaPath, expectedMetaPath);
+			log.off('MRT', 'Expected Metadata file created!');
+		} catch (err) {
+			/* istanbul ignore next */
+			youHaveBeenBad('🤔 Something went wrong while copying metadata - Check actual folder', err);
+		}
+	}
+
+	return getMetadatDiff(expectedMetaPath, actualMetaPath);
 };
 
 const fetchMetadata = urlToGetMetadataFrom => {
-		return new Promise((resolve, reject) => {
-				scrape(urlToGetMetadataFrom)
-						.then(metadata => resolve(metadata))
-						.catch(err => {
-								reject(new Error(`Unable to retrieve metadata for ${urlToGetMetadataFrom} => ${err}`));
-						});
-		});
+	return new Promise((resolve, reject) => {
+		scrape(urlToGetMetadataFrom)
+			.then(metadata => resolve(metadata))
+			.catch(err => {
+				reject(new Error(`Unable to retrieve metadata for ${urlToGetMetadataFrom} => ${err}`));
+			});
+	});
 };
 
 const setOptions = desiredOptions => {
-		if (!(desiredOptions instanceof Object)) {
-				youHaveBeenBad(`setOptions: options is expected to be an Object. Got ${typeof desiredOptions} - ${desiredOptions}`);
-		}
+	if (!(desiredOptions instanceof Object)) {
+		youHaveBeenBad(`setOptions: options is expected to be an Object. Got ${typeof desiredOptions} - ${desiredOptions}`);
+	}
 
-		const defaultOptions = {
-				path: METADATA_PATH,
-				keysOnly: false
-		};
+	const defaultOptions = {
+		path: METADATA_PATH,
+		keysOnly: false
+	};
 
-		options = Object.assign(defaultOptions, desiredOptions);
+	options = Object.assign(defaultOptions, desiredOptions);
 };
 
 /**
@@ -129,23 +138,23 @@ const setOptions = desiredOptions => {
  * @return {Boolean} whether or not a match was found
  */
 exports.mrt = (opts, ...urls) => {
-		const urlPromises = urls.map(fetchMetadata);
-		const metaDataResults = [];
+	const urlPromises = urls.map(fetchMetadata);
+	const metaDataResults = [];
 
-		setOptions(opts);
+	setOptions(opts);
 
-		return Promise
-				.all(urlPromises)
-				.then(results => {
-						results.forEach(metadata => {
-								metaDataResults.push(metadata);
-						});
-						const urlkeyAndMetadataValuePairs = urls.reduce((obj, k, i) => Object.assign(obj, {[k]: metaDataResults[i]}), {});
-						return generateMetaFiles(urlkeyAndMetadataValuePairs);
-				})
-				.catch(err => {
-						youHaveBeenBad(err);
-				});
+	return Promise
+		.all(urlPromises)
+		.then(results => {
+			results.forEach(metadata => {
+				metaDataResults.push(metadata);
+			});
+			const urlkeyAndMetadataValuePairs = urls.reduce((obj, k, i) => Object.assign(obj, {[k]: metaDataResults[i]}), {});
+			return generateMetaFiles(urlkeyAndMetadataValuePairs);
+		})
+		.catch(err => {
+			youHaveBeenBad(err);
+		});
 };
 
 /**
@@ -155,11 +164,11 @@ exports.mrt = (opts, ...urls) => {
  * @param {String} [diffboardOptions.diffboardTitle]
  */
 exports.diffboard = diffboardOptions => {
-		const defaultOptions = {
-				path: METADATA_PATH
-		};
+	const defaultOptions = {
+		path: METADATA_PATH
+	};
 
-		options = Object.assign(defaultOptions, diffboardOptions);
-		const diffboard = new Diffboard(options);
-		diffboard.generateHtml();
+	options = Object.assign(defaultOptions, diffboardOptions);
+	const diffboard = new Diffboard(options);
+	diffboard.generateHtml();
 };
